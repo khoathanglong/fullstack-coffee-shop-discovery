186279af8bd6e40f9680a53407a33f6bac3bca7e
asentaja <Asentaja@BC-1K6MSJ2.bc.local>
2018-04-28 13:30:05 +0300

    update cover

--- src/cover.js
+++ src/cover.js
@@ -1,25 +1,33 @@
 import React from 'react'
-import {FormControl,InputGroup,Button} from 'react-bootstrap'
+import {FormControl,InputGroup,Button,ControlLabel,Row, Col} from 'react-bootstrap'
 
 export default ()=>{
 	return (
 		<div className="cover" >
-			<div>
-				<InputGroup>
-				 	<FormControl 
-			          	type="text"
-			           	placeholder="City Name"
-		          	/>
-				 	<InputGroup.Addon>
-				 		<i class="fa fa-search"></i>
-				 	</InputGroup.Addon>
-				</InputGroup><br/>
-				<span style={{color:'white',fontSize:'2em'}}>Or</span><br/><br/>
-				<Button bsStyle="success">Get your location{' '}
-					<i class="fa fa-map-marker" aria-hidden="true"></i>
-				</Button>
-			</div>
-			 
+		<div className="title">Discover the most popular Café</div>
+			<Row className="inputGroup">
+				<Col sm={5} smOffset={2} xs={12} >
+					<InputGroup>
+					 	<FormControl 
+				          	type="text"
+				           	placeholder="By City"
+			          	/>
+					 	<InputGroup.Addon>
+					 		<i class="fa fa-search"></i>
+					 	</InputGroup.Addon>
+					</InputGroup>
+				</Col>
+				<Col sm={1} className="<or></or>">
+					<span >
+						Or
+					</span>
+				</Col>
+				<Col sm={4} xs={12} >
+					<Button bsStyle="success">Your Location{' '}
+						<i class="fa fa-map-marker" aria-hidden="true"></i>
+					</Button>
+				</Col>
+			</Row>	 
         </div>
 	)
 }
